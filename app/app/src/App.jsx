471867e237924c985dcd50371cb51cc9a62e5c10
LUCAS\monto <lucasmontoby@gmail.com>
2024-02-14 20:07:55 -0300

    subo cambios

--- app/app/src/App.jsx
+++ app/app/src/App.jsx
@@ -1,124 +1,140 @@
 import { useState } from 'react'
+
 import './App.css'
 
-const Form = (addTask) =>{
-  cosnt [newTask, setNewTask] = useState()
 
-  const handleChange = (e) =>{
+const Form = ({addTask}) => {
+  const [newTask, setNewTask] = useState()
+
+  const handleChange = (e) => {
     setNewTask(e.target.value)
   }
 
-  const handleSubmit = (e) =>{
+  const handleSubmit = (e) => {
     e.preventDefault()
     addTask(newTask)
   }
-
   return(
-    <form onSubmit={(e) => handleSubmit(e)} >
-      <label >Tarea
-        <input type="text" name="task" placeholder='Ingresa tarea' onChange={handleChange}/>
-      </label>
-
-      <button type="submit">Agregar</button>
-    </form>
-  )
-}
-
-
-const Task = ({ id, task, state, completeTask, deleteTask, editTask }) =>{
-
-    const handleCompleteTask  = (e) => {
-      completeTask(e)
-    }
-
-    const handleDeleteTask = (e) =>{
-      deleteTask(e)
-    }
-
-    
-    const handleEditTask = (e) =>{
-     
-      editTask(e)
-    }
-
-  return(
-      <li id={id} >
-        {task}
-
-        <div>
-
-          <input id={ ` Estado${id}`} onChange={(e) => handleCompleteTask(e) } type="checkbox"
-          value={state}
+    <form onSubmit={(e) => handleSubmit(e) }>
+      <label >
+          Tarea
+          <input type="text" name="task" placeholder="ingrese tarea..."
+          onChange={handleChange}
           />
+      </label>
+      <button type="submit">Agregar</button>
+  </form>
 
-          <button oClick={handleDeleteTask} id={`Eliminar ${id}`}>Eliminar</button>
-          <button oClick={handleEditTask} id={`editar ${id}`}>Editar</button>
+  )
+  
+}
 
-        </div>
-      </li>
+const Tasks = ({tasks, completeTask, deleteTask, editTask}) => {
+  return (
+    <section id="list-tasks">
+      {
+        tasks.length > 0 ? 
+          <ul id="tasks">
+            {
+                tasks.map(task => (
+                    <Task {...task} completeTask={completeTask} deleteTask={deleteTask} editTask={editTask} key={task.id}/>
+                  ))
+            }  
+          </ul>
+        :
+        <p >NO hay tareas</p>
+      
+      }
+    </section>
   )
 }
 
-const Tasks = ({tasks, completeTask, deleteTask, editTask})=>{
+const Task = ({id,task, state, completeTask, deleteTask, editTask}) => {
+
+  const handleCompleteTask = (e) => {
+    completeTask(e)
+  }
+  const handleDeleteTask = (e) => {
+    deleteTask(e)
+  }
+  const handleEditTask = (e) => {
+    editTask(e)
+  }
 
   return(
-    <div>
-      {
-        task.length > 0 ?
-        <ul>
-          {
-            tasks.map(task => (
-              <Task {...task} completeTask = {completeTask} deleteTask={deleteTask} editTask = {editTask} key={task.id}/>
-            ))
-          }
-        </ul>
-        : 
-        <p>No hay tareas</p>
-      }
-
-    </div>
+    <li className="task" id={id}>
+            {task}
+            <div className='task_controls'>
+                <input id={`status-${id}`} onChange={(e) => handleCompleteTask(e)}  type="checkbox" name='task_status'
+                value={state}
+                />
+                <button onClick={handleDeleteTask} id={`delete-${id}`}></button>
+                <button onClick={handleEditTask} id={`edit-${id}`}></button>
+            </div>
+        </li>
   )
 }
 
-function App (){
-  const  [tasks, setTasks] = useState([])
 
-  const addTask = (task) =>{
-      let newtask = {
-          id: id,
-          task: task,
-          state: false
-      }
-      setTasks([...tasks, newtask])
+function App() {
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (task) => {
+    let newtask = {
+        id: Math.floor( Math.random() * 100) + 1,
+        task: task,
+        state: false
+    }
+    console.log(newtask);
+    setTasks([...tasks, newtask])
   }
 
-  const completeTask = (e) =>{
-    let id = e.target.id.slice(5)
-    const updateTask = tasks.map(task => task.id === id ? {...task,  state: !task.state}: task)
-    setTasks(updateTask)
+
+  const completeTask = (e) => {
+    let id = e.target.id
+    id = id.slice(7)
+    let updateTasks = tasks.map(task => {
+        if (task.id == id) {
+            return {...task, state: !task.state}
+        }
+        return task
+    })
+    setTasks(updateTasks)
   }
 
-  const deleteTask = (e)=>{
-    let id = e.target.id.slice(5)
-    const updateTask = tasks.filter(task => task.id !== id)
-    setTasks(updateTask)
+  const deleteTask = (e) => {
+    let htmlElement = e.target.id 
+    let id = htmlElement.slice(7)
+            console.log(id);
+    const updateTasks = tasks.filter( task => task.id != id)
+    setTasks(updateTasks)
   }
 
   const editTask = (e) => {
-    let id = e.target.id.slice(5)
-    const task =  tasks.find(task => task.id == id)
-    const edit = prompt( "Nueva Tarea", task.task);
-    const updateTask = tasks.map(tarea => tarea.id === id  ? { ...task , task:edit}: tarea)
-    setTasks(updateTask)
+    let htmlElement = e.target.id
+    let id = htmlElement.slice(5)
+    let task = tasks.find( task => task.id == id)
+    let edit = prompt('edit', task.task)
+    const updateTasks = tasks.map(tarea => {
+        if (tarea.id == id) {
+            return {...tarea, task:edit}
+        }
+        return tarea
+    })
+    console.log(updateTasks);
+    setTasks(updateTasks)
   }
-
-
-
-  return(
+  return (
     <>
-
+      <header>
+          <h1>Lista de Tareas</h1>
+      </header>
+      <main>
+          <Form addTask={addTask}/>
+          <Tasks tasks={tasks} completeTask={completeTask} deleteTask={deleteTask} editTask={editTask}/>
+      </main>
     </>
   )
 }
 
-export default App;
+export default App
